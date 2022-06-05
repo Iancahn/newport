77d87af5a958792ab8fe83826761def792f3cf56
Ian Cahn <iancahn@live.com>
2022-06-05 15:11:40 +0200

    First Page Completed
    Added the FAQ section, which brings the  first page to completion.

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+//Import Icons
+import clock from '../img/clock.svg';
+import diaphragm from '../img/diaphragm.svg';
+import money from '../img/money.svg';
+import teamwork from '../img/teamwork.svg'
+import home2 from '../img/home2.png'
 
 const ServicesSection = () => {
     return (
         <div className="services">
-
+            <div className="description">
+                <h2>High <span>quality</span> services</h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon">
+                            <img src={clock} alt="a clock icon"></img>
+                            <h3>Efficient</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={teamwork} alt="a teamwork icon"></img>
+                            <h3>Teamwork</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={diaphragm} alt="a diaphragm icon"></img>
+                            <h3>Diapgragm</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={money} alt="a money icon"></img>
+                            <h3>Affordable</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="image">
+                <img src={home2} alt="a high quality camera"></img>
+            </div>
         </div>
-    )
+    );
 }
 
 export default ServicesSection;

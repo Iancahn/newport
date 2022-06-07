39aa4f4eed10e55c7f0f68a68f3ab76fca150d3a
Ian Cahn <iancahn@live.com>
2022-06-07 20:44:44 +0200

    React Router V6.3
    Updated from Switch to Routes/Route methods. Also learned about exact paths. As well as replacing Hrefs with Link in Navs, after inporting Link from ReactRouter

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import React from 'react';
 import Globalstyle from './components/GlobalStyle';
 //Import Pages
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
+// Router
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <AboutUs />
       <Globalstyle />
+      <Nav />
+      <Routes>
+        <Route path='/' exact element={<AboutUs />} />
+        <Route path='/aboutus' exact element={<AboutUs />} />
+        <Route path='/ourwork' exact element={<OurWork />} />
+        <Route path='/contactus' exact element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }

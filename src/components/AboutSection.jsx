ab36396da4c94063b33716e4933cf7cbb0ea6c09
Ian Cahn <iancahn@live.com>
2022-06-09 21:27:57 +0200

    Added Animation component
    Adding a couple of animations.  Learned about exit animations in Framer. Where you need to import AnimatePresence from FM and then wrap your Routes inside AnimatePresence. The exitBeforeEnter also tells React to wait for our component exiting to finish its animation before starting the new component animations

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,34 +7,39 @@ import { motion } from 'framer-motion';
 
 const AboutSection = () => {
 
-    const titleAnim = {
-        hidden: { opacity: 0 },
-        show: { opacity: 1, transition: { duration: 2 } },
-    };
-    const container = {
-        hidden: { x: 100 },
-        show: {
-            x: 0,
-            transition: {
-                duration: 0.75,
-                ease: 'easeOut',
-                staggerChildren: 0.5,
-                when: "afterChildren"
-            }
-        },
-    };
+    // const titleAnim = {
+    //     hidden: { opacity: 0 },
+    //     show: { opacity: 1, transition: { duration: 2 } },
+    // };
+    // const container = {
+    //     hidden: { x: 100 },
+    //     show: {
+    //         x: 0,
+    //         transition: {
+    //             duration: 0.75,
+    //             ease: 'easeOut',
+    //             staggerChildren: 0.5,
+    //             when: "afterChildren"
+    //         }
+    //     },
+    // };
     return (
         <StyledAbout>
             <StyledDescription>
-                <motion.div variants={container} initial="hidden" animate="show" className="title">
+                <motion.div
+                // variants={container}
+                // initial="hidden"
+                // animate="show"
+                // className="title"
+                >
                     <StyledHide>
-                        <motion.h2 variants={titleAnim}>I work to make</motion.h2>
+                        <motion.h2>I work to make</motion.h2>
                     </StyledHide>
                     <StyledHide>
-                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
+                        <motion.h2>your <span>dreams</span> come</motion.h2>
                     </StyledHide>
                     <StyledHide>
-                        <motion.h2 variants={titleAnim}>true.</motion.h2>
+                        <motion.h2>true.</motion.h2>
                     </StyledHide>
                 </motion.div>
                 <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
 import { MovieState } from '../movieState';
 import { wrapHandler } from 'framer-motion';
+// Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
 
 const MovieDetail = () => {
     const url = useLocation();
@@ -19,7 +22,7 @@ const MovieDetail = () => {
     return (
         <>
             {movie && (
-                <StyledDetails>
+                <StyledDetails variants={pageAnimation} initial="hidden" animate="show" exit="exit">
                     <StyledHeadline>
                         <h2>{movie.title}</h2>
                         <img src={movie.mainImg} alt="movie" />
@@ -40,7 +43,7 @@ const MovieDetail = () => {
 
 };
 
-const StyledDetails = styled.div`
+const StyledDetails = styled(motion.div)`
     color:white;
 `
 

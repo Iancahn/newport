1311b576ed6f7f4e28fa4ecddc0e2a5ac784a00c
Ian Cahn <iancahn@live.com>
2022-06-09 21:40:58 +0200

    More Animations
    Added titleAnim, fade and photAnim.

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,49 +4,29 @@ import home1 from '../img/home1.png'
 import { StyledAbout, StyledDescription, StyledImage, StyledHide } from "../styles";
 // Framer Motion
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
 
 const AboutSection = () => {
 
-    // const titleAnim = {
-    //     hidden: { opacity: 0 },
-    //     show: { opacity: 1, transition: { duration: 2 } },
-    // };
-    // const container = {
-    //     hidden: { x: 100 },
-    //     show: {
-    //         x: 0,
-    //         transition: {
-    //             duration: 0.75,
-    //             ease: 'easeOut',
-    //             staggerChildren: 0.5,
-    //             when: "afterChildren"
-    //         }
-    //     },
-    // };
     return (
         <StyledAbout>
             <StyledDescription>
-                <motion.div
-                // variants={container}
-                // initial="hidden"
-                // animate="show"
-                // className="title"
-                >
+                <motion.div>
                     <StyledHide>
-                        <motion.h2>I work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>I work to make</motion.h2>
                     </StyledHide>
                     <StyledHide>
-                        <motion.h2>your <span>dreams</span> come</motion.h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </StyledHide>
                     <StyledHide>
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </StyledHide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </StyledDescription>
             <StyledImage>
-                <img src={home1} alt='Guy with a camera'></img>
+                <motion.img src={home1} alt='Guy with a camera' variants={photoAnim}></motion.img>
             </StyledImage>
         </StyledAbout>
     );
